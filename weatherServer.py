03650301232dbbca3ed48e8860024556b92650ab
Spikey Griffiths <jonathan@rosepip.com>
2019-10-03 16:54:12 +0100

    Time as text, etc

--- weatherServer.py
+++ weatherServer.py
@@ -51,7 +51,7 @@ def owmToSym(symbolVar):
     return sym.get(symbolVar, Sym.Unknown)
 
 def GetWorstWeather(detail):
-    global symText, symSym, severestGroup, severestSub
+    global cloudText, symSym, severestGroup, severestSub
     global maxTemp, minTemp
     global maxWind, windText, windDir
     if detail.tag == "symbol":
@@ -65,31 +65,31 @@ def GetWorstWeather(detail):
             if symbolSub>severestSub:
                 severestSub=symbolSub
                 symSym=owmToSym(symbolVar)
-                symText=symbolText
+                cloudText=symbolText
         else:
             if symbolGroup==2: # Thunderstorms
                 severestGroup=symbolGroup # Nothing beats thunderstorms!
                 severestSub=symbolSub
                 symSym=owmToSym(symbolVar)
-                symText=symbolText
+                cloudText=symbolText
             elif symbolGroup==3: # Drizzle
                 if severestGroup==8: # Drizzle beats clouds
                     severestGroup=symbolGroup
                     severestSub=symbolSub
                     symSym=owmToSym(symbolVar)
-                    symText=symbolText
+                    cloudText=symbolText
             elif symbolGroup==5: # Rain
                 if severestGroup==8 or severestGroup==2: # Rain beats clouds & drizzle
                     severestGroup=symbolGroup
                     severestSub=symbolSub
                     symSym=owmToSym(symbolVar)
-                    symText=symbolText
+                    cloudText=symbolText
             elif symbolGroup==6:
                 if severestGroup==8 or severestGroup==2 or severestGroup==5: # Snow beats clouds, drizzle or rain
                     severestGroup=symbolGroup
                     severestSub=symbolSub
                     symSym=owmToSym(symbolVar)
-                    symText=symbolText
+                    cloudText=symbolText
     if detail.tag == "temperature":
         low = float(detail.attrib["min"]) - 273.15 # Convert Kelvin to Celsius
         high = float(detail.attrib["max"]) - 273.15
@@ -108,13 +108,13 @@ def GetWorstWeather(detail):
         windDir = dir # Might want to try averaging wind direction, rather than just getting last one?
 
 def SetDefaultForecast():
-    global symText, symSym, severestSymbol, severestGroup, severestSub
+    global cloudText, symSym, severestSymbol, severestGroup, severestSub
     global maxTemp, minTemp
     global maxWind, windText, windDir
     severestSymbol = 800 # 800, Clear sky by default
     severestGroup = int(severestSymbol/100)
     severestSub = severestSymbol % 100
-    symText = "Clear sky"
+    cloudText = "Clear sky"
     symSym = owmToSym("01d") # Assume clear day sky
     minTemp = 50.0 # Silly low temp
     maxTemp = -20.0 # Silly high temp
@@ -127,7 +127,7 @@ def GetForecastSlot(forecastSlot):
 
 def GetWeatherForecast():
     global forecastPeriod
-    global symText, symSym, severestSymbol, severestGroup, severestSub
+    global cloudText, symSym, severestSymbol, severestGroup, severestSub
     global maxTemp, minTemp
     global maxWind, windText, windDir
     forecastPeriod = "N/A"
@@ -154,20 +154,59 @@ def GetWeatherForecast():
         severestSymbol = severestGroup * 100 + severestSub
         # ToDo: Convert severestSymbol from OWM to Sym format
 
+def GetTimeInWords():
+    numbers = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve']
+    hours = datetime.now().hour % 12
+    minutes = datetime.now().minute
+    if minutes >= 33: hours = hours + 1 # For "Quarter to ..."
+    if hours > 12: hours = 1
+    hourTxt = numbers[hours]
+    if (minutes > 58) or (minutes < 3):
+        return hourTxt + " o'clock"
+    elif minutes < 8:
+        return "Five past " + hourTxt
+    elif minutes < 13:
+        return "Ten past " + hourTxt
+    elif minutes < 18:
+        return "Quarter past " + hourTxt
+    elif minutes < 23:
+        return "Twenty past " + hourTxt
+    elif minutes < 28:
+        return "Twenty Five past " + hourTxt
+    elif minutes < 33:
+        return "Half past " + hourTxt
+    elif minutes < 38:
+        return "Twenty Five to " + hourTxt
+    elif minutes < 43:
+        return "Twenty to " + hourTxt
+    elif minutes < 48:
+        return "Quarter to " + hourTxt
+    elif minutes < 53:
+        return "Ten to " + hourTxt
+    elif minutes < 58:
+        return "Five to " + hourTxt
+    return "Can't get here!"
+
 def MakeText():
     global forecastPeriod
-    global symText, symSym, severestSymbol, severestGroup, severestSub
+    global cloudText, symSym, severestSymbol, severestGroup, severestSub
     global maxTemp, minTemp
     global maxWind, windText, windDir
     weatherDict = dict()
     weatherDict["period"] = forecastPeriod
     weatherDict["icon"] = str(symSym)[4:]
-    weatherDict["synopsis"] = symText+" & "+windText
+    weatherDict["cloudText"] = cloudText
     weatherDict["maxTemp"] = str(round(maxTemp))+"C"
     weatherDict["minTemp"] = str(round(minTemp))+"C"
     weatherDict["windSpeed"] = str(round(maxWind))
     weatherDict["windDir"] = str(windDir)
-    weatherDict["timeDigits"] = str(datetime.now().strftime("%H:%M"))
+    weatherDict["windText"] = windText
+    now = datetime.now()
+    weatherDict["timeDigits"] = str(now.strftime("%H:%M"))
+    weatherDict["timeText"] = GetTimeInWords()
+    weatherDict["dayOfWeekText"] = str(now.strftime("%A"))
+    weatherDict["dayOfMonthText"] = str(int(now.strftime("%d")))# Use int() to remove leading zero
+    weatherDict["monthText"] = str(now.strftime("%B"))
     return str(weatherDict)  # Ready for sending via socket to client
 
 def UpdateText():
@@ -176,7 +215,7 @@ def UpdateText():
 
 # Main entry point
 global forecastPeriod
-global symText, symSym
+global cloudText, symSym
 global maxTemp, minTemp
 global maxWind, windText
 s = socket.socket() #socket.AF_INET, socket.SOCK_STREAM)
